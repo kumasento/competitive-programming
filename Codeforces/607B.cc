5bf5f4e6868eb7b0700e78e5b2ae6224ef186bf8
Ruizhe <vincentzhaorz@gmail.com>
2021-05-29 11:33:50 +0100

    CF Daily

---
+++ Codeforces/607B.cc
@@ -0,0 +1,66 @@
+/**
+ * Author: kumasento
+ * Date:   2021-05-29T10:52:04
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/// --------------------- Types:
+using LL = long long;
+using PII = pair<int, int>;
+using PIL = pair<int, LL>;
+using PLI = pair<LL, int>;
+using PLL = pair<LL, LL>;
+
+/// --------------------- Constants:
+constexpr int INF = 1e9;
+constexpr LL LINF = 1e16;
+constexpr LL MOD = 1e9 + 7;
+
+/// --------------------- Helpers:
+// *** Credit to Neal Wu
+// overload operator<<() for pairs.
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// overload container output. Won't affect strings.
+template <typename C, typename T = typename enable_if<!is_same<C, string>::value, typename C::value_type>::type>
+ostream &operator<<(ostream &os, const C &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// variadic debugger.
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+/// --------------------- Globals:
+
+/// --------------------- Solution:
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  #ifdef LOCAL_DEBUG
+  ifstream in("607B.in"); if (in) cin.rdbuf(in.rdbuf());
+  #endif
+
+
+  int n; cin >> n;
+  vector<int> c(n); for (int &i : c) cin >> i;
+
+
+  vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+  for (int i = 0; i < n; i ++) dp[i][i] = 0, dp[i][i+1] = 1;
+  for (int len = 2; len <= n; len ++)
+    for (int i = n - len; i >= 0; i --) {
+      int j = i + len;
+      dp[i][j] = dp[i+1][j] + 1; // worst case
+      if (c[i] == c[i+1]) dp[i][j] = min(dp[i+2][j] + 1, dp[i][j]);
+      for (int k = i + 2; k < j; k ++) 
+        if (c[i] == c[k])
+          dp[i][j] = min(dp[i][j], dp[i+1][k] + dp[k+1][j]);
+    }
+
+  cout << dp[0][n] << endl;
+}
