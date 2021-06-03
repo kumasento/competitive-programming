6e026f3fe8c3fd63879a3d3eb88945e99b057185
Ruizhe <vincentzhaorz@gmail.com>
2021-06-03 22:34:56 +0100

    ABC 194

---
+++ AtCoder/ABC_194/B.cc
@@ -0,0 +1,62 @@
+/**
+ * Author: kumasento
+ * Date:   2021-06-03T21:19:08
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/// --------------------- Types:
+using LL = long long;
+using PII = pair<int, int>;
+using PIL = pair<int, LL>;
+using PLI = pair<LL, int>;
+using PLL = pair<LL, LL>;
+
+/// --------------------- Constants:
+constexpr int INF = 1e9;
+constexpr LL LINF = 1e16;
+constexpr LL MOD = 1e9 + 7;
+
+/// --------------------- Helpers:
+// *** Credit to Neal Wu
+// overload operator<<() for pairs.
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// overload container output. Won't affect strings.
+template <typename C, typename T = typename enable_if<!is_same<C, string>::value, typename C::value_type>::type>
+ostream &operator<<(ostream &os, const C &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// variadic debugger.
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+/// --------------------- Globals:
+
+/// --------------------- Solution:
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  #ifdef LOCAL_DEBUG
+  ifstream in("B.in"); if (in) cin.rdbuf(in.rdbuf());
+  #endif
+
+  int N; cin >> N;
+  vector<int> A(N), B(N); for (int i = 0; i < N; i ++) cin >> A[i] >> B[i];
+
+  int ans = INT_MAX;
+  for (int i = 0; i < N; i ++) {
+    ans = min(ans, A[i] + B[i]);
+    for (int j = 0; j < N; j ++) {
+      if (i == j) continue;
+      ans = min(ans, max(A[i], B[j]));
+    }
+  }
+
+  cout << ans << '\n';
+
+}
